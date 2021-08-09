af35535dea786bafad1fe5d41c038fd87c5cd11b
shivam2891 <shivam.shah2891@gmail.com>
2021-08-09 18:44:56 -0400

    short commit demo for Shivam

--- MessagingApp.API/Startup.cs
+++ MessagingApp.API/Startup.cs
@@ -26,6 +26,7 @@ namespace MessagingApp.API
  
 
         // This method gets called by the runtime. Use this method to add services to the container.
+		// Some changes
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => 
